93af8caacaa66fa5aee3fa05c99ed5de9bfbedbd
Pam <52968147+Pamela-anthonydas@users.noreply.github.com>
2019-12-01 23:13:12 -0500

    changes to marker icon images

--- KidsRide-mapboxglV2/static/js/logic1.js
+++ KidsRide-mapboxglV2/static/js/logic1.js
@@ -12,11 +12,44 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Store API query variables
-// var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
-// var date = "$where=created_date between'2016-01-10T12:00:00' and '2017-01-01T14:00:00'";
-// var complaint = "&complaint_type=Rodent";
-// var limit = "&$limit=10000";
+
+
+var greenIcon = new L.Icon({
+  //iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  iconUrl:'http://www.clker.com/cliparts/9/c/E/w/X/r/ballerina-purple-hi.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [40, 60],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var RedIcon = new L.Icon({
+  //iconUrl: 'static/images/music2.png',
+  iconUrl:'http://www.clker.com/cliparts/6/4/1/8/1206581728647714433papapishu_guitar_5.svg.hi.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+   iconSize: [30, 50],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+//L.AwesomeMarkers.Icon.prototype.options.prefix = 'ion';
+// var RedIcon = L.AwesomeMarkers.Icon({
+//   icon: 'music',
+//   markerColor: 'red'
+// });
+
+var BlueIcon = new L.Icon({
+  //iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+  iconUrl:'http://www.clker.com/cliparts/d/1/4/6/11971185311926706097Gioppino_Basketball.svg.hi.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 
 // Assemble API query URL
 var url = "/data";
@@ -28,23 +61,54 @@ d3.json(url, function(response) {
   var markers = L.markerClusterGroup();
 
   // Loop through data
+  console.log(response)
   for (var i = 0; i < response.length; i++) {
 
     // Set the data location property to a variable
     var location = response[i].location;
 
-    var cat = response[i].category;
+    var cat = response[i].type;
 
-    //if(cat==="Dance"){
+    if(cat==="Dance Classes"){
 
     // Check for location property
       if (location) {
 
         // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.marker([location[0], location[1]])
+        markers.addLayer(L.marker([location[0], location[1]],{icon: greenIcon})
           .bindPopup(response[i].name,response[i].address));
       }
-    //}
+    }
+
+    else if(cat==="Music Classes"){
+
+      // Check for location property
+        if (location) {
+  
+          // Add a new marker to the cluster group and bind a pop-up
+        //   markers.addLayer(L.marker([location[0], location[1]],{
+        //     icon: L.AwesomeMarkers.icon({
+        //         icon: 'help-buoy',
+        //         markerColor: 'red'
+        //     })
+        // })
+        //     .bindPopup(response[i].name,response[i].address));
+        markers.addLayer(L.marker([location[0], location[1]],{icon: RedIcon})
+        .bindPopup(response[i].name,response[i].address));
+        }
+      }
+
+    else {
+      if (location) {
+  
+        // Add a new marker to the cluster group and bind a pop-up
+        markers.addLayer(L.marker([location[0], location[1]],{icon: BlueIcon})
+          .bindPopup(response[i].name,response[i].address));
+      }
+
+    }
+
+    
 
   }
 
