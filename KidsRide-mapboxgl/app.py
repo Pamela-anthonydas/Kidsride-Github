9a38f057cfc802109bfc1bfc67b28323e78e7f9e
SwethaVikas <shwethavikas@gmail.com>
2019-12-09 23:33:02 -0500

    Pushed final commit to Swetha Vikas
    Done

--- KidsRide-mapboxgl/app.py
+++ KidsRide-mapboxgl/app.py
@@ -6,74 +6,33 @@ import json
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/KidsRidedb1"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/KidsRidedb3"
 mongo = PyMongo(app)
 
-#mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
-
-# Or set inline
-
-
-
+# Home route to render home page
 @app.route("/")
 def index():
-    #mars_data = mongo.db.collection.find_one()
     return render_template("index.html")
-	
-	#destination_data = mongo.db.collection.find_one()
-	#return render_template("index.html", vacation=destination_data)
-    
-@app.route("/map")
+
+#  /map  route to render marker cluster on map based on clusterslogic.js   
+@app.route("/map",endpoint='map')
 def map():
     return render_template("Marker_Clusters.html")
 
+#  /directions   route to render driving directions on map based on directionslogic.js 
+@app.route("/directions",endpoint='directions')
+def directions():
+    return render_template("directions.html")
+
+# /data route to return mongo db data to clusterslogic.js  and directionslogic.js 
 @app.route("/data")
 def data():
-    #mars_data  = mongo.db.mars_data 
-    # mars= scrape_mars.scrape()
-    # mongo.db.collection.update({}, mars, upsert=True)
-    # return redirect("/", code=302)
-
-    # names=mongo.db.activityData2.find( {} )
-    # #return render_template('Marker_Clusters.html', names=names)
-    # return jsonify(names)
-
-    # data = mongo.db.activityData2.find_one()
-    # return render_template('Marker_Clusters.html', data=data)
-    #return jsonify(data)
-    # db = client[KidsRidedb1]
-    # collection = db[activityData2]
-    # documents = activityData2.find()
-    documents = mongo.db.activityData2.find()
+    documents = mongo.db.activityData20.find()
     response = []
     for document in documents:
         #document['_id'] = str(document['_id'])
         response.append(document)
     return json.dumps(response)
-
-    # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    # mydb = myclient["KidsRidedb1"]
-    # mycol = mydb["activityData2"]
-
-    # for x in mycol.find({},{ "_id": 0, "name": 1, "address": 1 }):
-    #     print(x)
-
-    # for category,name,address,city,state, location,reviews, url,contactPhone in results:
-    #     kids_dict = {}
-    #     kids_dict["category"] = category
-    #     kids_dict["name"] = name
-    #     kids_dict["address"] = address
-    #     kids_dict["city"] = city
-    #     kids_dict["state"] = state
-    #     kids_dict["location"] = location
-    #     kids_dict["reviews"] = reviews
-    #     kids_dict["url"] = url
-    #     kids_dict["contactPhone"] = contactPhone
-    #     prcp_data.append(kids_dict)
-
-    # return jsonify(prcp_data)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
